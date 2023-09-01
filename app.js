7a8b08f0a0869f84cf4c010a0d7aef806ecd9c4c
idrees khan <midreeskhan7276@gmail.com>
2023-09-01 22:22:08 +0500

    done

---
+++ app.js
@@ -0,0 +1,142 @@
+
+
+                        ////// /////       chapter 1        /////////////////////////
+
+                        
+
+                        /////   Question  = 1   //////   
+
+
+// alert("Hello World")
+
+
+      
+
+                          /////   Question  = 2  //////
+
+
+// alert("Error ! Please enter invalid password")
+    
+
+                        
+                        /////   Question  = 3   //////
+
+
+
+// alert("welcome to js land.... \n happy coding")
+
+
+
+
+                       /////   Question  = 3   //////
+
+
+                       
+// alert("welcome to js land")
+// alert("happy coding")
+       
+
+
+                        /////   Question  = 4   //////
+
+
+       // alert("hello... i can run js through  my web browswe console")
+
+
+
+
+                      /////   Question  = 5 ,6 , 7   //////
+
+
+                      // sawal samjh m ni arha 
+
+     
+
+
+           //////////////////        chapter = 2         //////////////////////
+           
+                    /////   Question  = 1   //////  
+
+
+// var Username;
+         
+       
+          
+                     /////   Question  = 2   //////
+
+
+   // var name ="Idrees khan";
+    
+
+
+
+                  /////   Question  = 3   //////
+
+
+
+   // var js=" Hello World";
+// alert(js)
+
+
+
+
+                    /////   Question  = 4   //////
+
+                    
+
+   // var stdname ="idrees khan";
+// var stuage =19;
+// var  qualification="BMJ student";
+// alert(stdname)
+// alert(stuage)
+// alert(qualification)
+
+   
+
+
+ 
+                        /////   Question  = 5  //////
+ 
+
+
+// var pizza="pizza \n pizz\n piz \n pi \n p";
+// alert(pizza)
+
+
+
+
+
+                 /////   Question  = 6   //////
+
+
+
+
+      // var email=" midreeskhan7276@gmail.com";
+// alert("My Email Address is"+ email)
+
+
+
+                 /////   Question  = 7   //////
+
+
+  // var book = " A smater \n way to learn JavaScript";
+// alert("I am trying to learn from the Book " + book)
+  
+
+
+                 /////   Question  = 8   //////
+   
+
+
+// document.write("Yah! I can write HTML content through JavaScript")
+  
+
+
+
+                 /////   Question  = 9   //////
+
+
+
+
+// var line = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(line)
